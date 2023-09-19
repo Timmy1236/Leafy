54128db85e586d05bf5a972b4c3b8d554ff277be
Timmy1236 <timmy.1236@outlook.com>
2023-09-18 23:46:30 -0300

    Fixeando los slash y moviendolo a categorias

--- commands/slash-type/discord/avatar_slash.js
+++ commands/slash-type/discord/avatar_slash.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js")
 const { getColorFromURL } = require('color-thief-node');
-const config = require("../../config")
 module.exports = {
   data: new Discord.SlashCommandBuilder()
     .setName("avatar")
@@ -20,7 +19,7 @@ module.exports = {
     function rgbToHex(r, g, b) {
       return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
     }
-
+    asd
     if (interaction.options.getUser('usuario')) {
       const avatar = await interaction.options.getUser('usuario').displayAvatarURL({ dynamic: true, size: 1024 })
       const avatarWEBP = await interaction.options.getUser('usuario').displayAvatarURL({ extension: "webp", size: 512 })
@@ -33,7 +32,7 @@ module.exports = {
         .setDescription(`▸ 📷 Formatos\n> [WEBP](${avatarWEBP}) | [PNG](${avatarPNG}) | [JPG](${avatarJPG}) | [JPEG](${avatarJPEG}) \n\n▸ 🖌️ Color Dominante\n> HEX: ${rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2])}\n> RGB: ${dominantColor}\n\n▸ 📎 Fuentes\n>>> [Buscar en Google](https://lens.google.com/uploadbyurl?url=${avatar})\n[Buscar en Yandex](https://yandex.com/images/search?url=${avatar}&rpt=imageview)\n[Buscar en SauceNAO](https://saucenao.com/search.php?url=${avatar})`)
         .setColor(dominantColor)
         .setImage(avatar)
-        .setFooter({ text: `Obtén su banner: ${config.BOT.PREFIX}banner` })
+        .setFooter({ text: `Obtén su banner: ${client.prefix}}banner` })
 
       interaction.reply({ embeds: [avatarglobal] })
     } else {
@@ -48,7 +47,7 @@ module.exports = {
         .setDescription(`▸ 📷 Formatos\n> [WEBP](${avatarWEBP}) | [PNG](${avatarPNG}) | [JPG](${avatarJPG}) | [JPEG](${avatarJPEG}) \n\n▸ 🖌️ Color Dominante\n> HEX: ${rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2])}\n> RGB: ${dominantColor}\n\n▸ 📎 Fuentes\n>>> [Buscar en Google](https://lens.google.com/uploadbyurl?url=${avatar})\n[Buscar en Yandex](https://yandex.com/images/search?url=${avatar}&rpt=imageview)\n[Buscar en SauceNAO](https://saucenao.com/search.php?url=${avatar})`)
         .setColor(dominantColor)
         .setImage(avatar)
-        .setFooter({ text: `Obtén su banner: ${config.BOT.PREFIX}banner` })
+        .setFooter({ text: `Obtén su banner: ${client.prefix}banner` })
       interaction.reply({ embeds: [avatarglobal] })
     }
   }
