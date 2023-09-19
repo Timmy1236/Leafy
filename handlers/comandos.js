c4065a0454a3f4e9ee4cad4cff4f3f089312cda0
Timmy1236 <timmy.1236@outlook.com>
2023-09-18 23:45:19 -0300

    Haciéndolo lo mas "leíble" posible.

--- handlers/comandos.js
+++ handlers/comandos.js
@@ -1,41 +1,30 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const path = require("path");
 const ascii = require("ascii-table");
 const gradient = require('gradient-string');
-const array = require('../src/commandslist.js');
+
 let table = new ascii("Comandos");
 table.setHeading("Categoría", "Comando", "Estado");
 
 module.exports = client => {
-  console.log(gradient.fruit("Bot> Cargando comandos, espere..."))
-  client.cmd = new Discord.Collection();
-  fs.readdirSync("./commands/message-type").forEach((file) => {
-    if (fs.lstatSync(path.resolve("./commands/message-type", file)).isDirectory()) { }
-    let comando = fs
-      .readdirSync(`./commands/message-type/${file}/`)
-      .filter((f) => f.endsWith(".js"));
-    for (var modu of comando) {
-      let fileName = modu.substring(0, modu.length - 3);
-      let comandos = require(`../commands/message-type/${file}/${modu}`);
-      client.cmd.set(comandos.nombre, comandos);
-      if (comandos.nombre) {
+  try {
+    console.log(gradient.fruit("Bot> Cargando comandos, espere..."))
+    client.cmd = new Discord.Collection(); // Creamos una colección para almacenar los comandos.
+
+    fs.readdirSync("./commands/message-type").forEach((file) => {
+      let comando = fs.readdirSync(`./commands/message-type/${file}/`).filter((f) => f.endsWith(".js"));
+
+      for (var modu of comando) {
+        let comandos = require(`../commands/message-type/${file}/${modu}`); // Requerimos el archivo para obtener su información.
+        client.cmd.set(comandos.nombre, comandos); // Agregamos el comando a la colección de comandos.
+
+        let fileName = modu.substring(0, modu.length - 3); // Eliminamos el ".js" de los archivos para que la lista tenga un mejor aspecto.
         table.addRow(file, fileName, "LISTO!");
-        switch (file) {
-          case "bot":
-            array.pushToBot(fileName);
-            break;
-          case "discord":
-            array.pushToDiscord(fileName);
-            break;
-          case "owner":
-            break;
-          default:
-            table.addRow(file, fileName, "NO CATEGORY");
-            break;
-        }
       }
-    }
-  });
-  console.log(gradient.mind(table.toString()));
+    });
+    console.log(gradient.mind(table.toString()));
+  } catch (error) {
+    console.log(gradient('orange', 'red')('Bot> Error!'));
+    console.error(error);
+  }
 }
