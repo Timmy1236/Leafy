cb56a9a08411d74cc5b5b68d2c0a0df0a8e86e2d
Timmy <timmy.1236@outlook.com>
2024-02-01 19:22:53 -0300

    Forzando mención de usuario en /avatar y mejorando el código también

--- commands/message-type/discord/avatar.js
+++ commands/message-type/discord/avatar.js
@@ -7,27 +7,36 @@ module.exports = {
   tieneHelp: 0,
   run: async (Discord, client, message, args) => {
     message.channel.sendTyping();
-    let id = message.mentions.members.size > 0 ? message.mentions.users.first().id : undefined || args[0] || message.author.id
-    client.users.fetch(id).then(async us => {
-      const avatar = await us.displayAvatarURL({ dynamic: true, size: 1024 })
-      const avatarWEBP = await us.displayAvatarURL({ extension: "webp", size: 512 })
-      const avatarPNG = await us.displayAvatarURL({ extension: "png", size: 512 })
-      const avatarJPG = await us.displayAvatarURL({ extension: "jpg", size: 512 })
-      const avatarJPEG = await us.displayAvatarURL({ extension: "jpeg", size: 512 })
-      const dominantColor = await getColorFromURL(avatarPNG);
 
+    // Obtenemos el ID del usuario
+    let id = message.mentions.members.size > 0 ? message.mentions.users.first().id : undefined || args[0] || message.author.id
+
+    // Hacemos un fetch del usuario
+    client.users.fetch(id).then(async us => {
+      const avatar = await us.displayAvatarURL({ dynamic: true, size: 1024 }) // Avatar del usuario en cualquier formato dado por Discord
+
+      // Función para obtener el avatar con extensión
+      function getAvatarWithExtension(extension) {
+        return us.displayAvatarURL({ extension: extension, size: 512 })
+      }
+
+      // El color dominante del avatar
+      const dominantColor = await getColorFromURL(getAvatarWithExtension("png"));
+
+      // Función para convertir decimal a hexadecimal
       function componentToHex(c) {
         var hex = c.toString(16);
         return hex.length == 1 ? "0" + hex : hex;
       }
 
+      // Función para convertir RGB a HEX
       function rgbToHex(r, g, b) {
         return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
       }
 
       const avatarglobal = new Discord.EmbedBuilder()
         .setAuthor({ name: us.username, iconURL: avatar })
-        .setDescription(`▸ 📷 Formatos\n> [WEBP](${avatarWEBP}) | [PNG](${avatarPNG}) | [JPG](${avatarJPG}) | [JPEG](${avatarJPEG}) \n\n▸ 🖌️ Color Dominante\n> HEX: ${rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2])}\n> RGB: ${dominantColor}\n\n▸ 📎 Fuentes\n>>> [Buscar en Google](https://lens.google.com/uploadbyurl?url=${avatar})\n[Buscar en Yandex](https://yandex.com/images/search?url=${avatar}&rpt=imageview)\n[Buscar en SauceNAO](https://saucenao.com/search.php?url=${avatar})`)
+        .setDescription(`▸ 📷 Formatos\n> [WEBP](${getAvatarWithExtension("webp")}) | [PNG](${getAvatarWithExtension("png")}) | [JPG](${getAvatarWithExtension("jpg")}) | [JPEG](${getAvatarWithExtension("jpeg")}) \n\n▸ 🖌️ Color Dominante\n> HEX: ${rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2])}\n> RGB: ${dominantColor}\n\n▸ 📎 Fuentes\n>>> [Buscar en Google](https://lens.google.com/uploadbyurl?url=${avatar})\n[Buscar en Yandex](https://yandex.com/images/search?url=${avatar}&rpt=imageview)\n[Buscar en SauceNAO](https://saucenao.com/search.php?url=${avatar})`)
         .setColor(dominantColor)
         .setImage(avatar)
       return message.reply({ embeds: [avatarglobal] })
--- commands/slash-type/discord/avatar.js
+++ commands/slash-type/discord/avatar.js
@@ -3,50 +3,42 @@ const { getColorFromURL } = require('color-thief-node');
 module.exports = {
   data: new Discord.SlashCommandBuilder()
     .setName("avatar")
-    .setDescription("Obtén el avatar de un usuario o el propio tuyo")
+    .setDescription("Obtén el avatar de un usuario que menciones")
     .addUserOption(option => option
       .setName('usuario')
       .setDescription('El usuario que quieras obtener su avatar')
-      .setRequired(false)
+      .setRequired(true)
     ),
   categoria: "Discord",
   async run(client, interaction) {
 
+    // Función para obtener el avatar con extensión
+    function getAvatarWithExtension(extension) {
+      return interaction.options.getUser('usuario').displayAvatarURL({ extension: extension, size: 512 })
+    }
+
+    // El color dominante del avatar
+    const dominantColor = await getColorFromURL(getAvatarWithExtension("png"));
+
+    // Función para convertir decimal a hexadecimal
     function componentToHex(c) {
       var hex = c.toString(16);
       return hex.length == 1 ? "0" + hex : hex;
     }
 
+    // Función para convertir RGB a HEX
     function rgbToHex(r, g, b) {
       return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
     }
-    if (interaction.options.getUser('usuario')) {
-      const avatar = await interaction.options.getUser('usuario').displayAvatarURL({ dynamic: true, size: 1024 })
-      const avatarWEBP = await interaction.options.getUser('usuario').displayAvatarURL({ extension: "webp", size: 512 })
-      const avatarPNG = await interaction.options.getUser('usuario').displayAvatarURL({ extension: "png", size: 512 })
-      const avatarJPG = await interaction.options.getUser('usuario').displayAvatarURL({ extension: "jpg", size: 512 })
-      const avatarJPEG = await interaction.options.getUser('usuario').displayAvatarURL({ extension: "jpeg", size: 512 })
-      const dominantColor = await getColorFromURL(avatarPNG);
-      const avatarglobal = new Discord.EmbedBuilder()
-        .setAuthor({ name: interaction.options.getUser('usuario').username, iconURL: avatar })
-        .setDescription(`▸ 📷 Formatos\n> [WEBP](${avatarWEBP}) | [PNG](${avatarPNG}) | [JPG](${avatarJPG}) | [JPEG](${avatarJPEG}) \n\n▸ 🖌️ Color Dominante\n> HEX: ${rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2])}\n> RGB: ${dominantColor}\n\n▸ 📎 Fuentes\n>>> [Buscar en Google](https://lens.google.com/uploadbyurl?url=${avatar})\n[Buscar en Yandex](https://yandex.com/images/search?url=${avatar}&rpt=imageview)\n[Buscar en SauceNAO](https://saucenao.com/search.php?url=${avatar})`)
-        .setColor(dominantColor)
-        .setImage(avatar)
 
-      interaction.reply({ embeds: [avatarglobal] })
-    } else {
-      const avatar = await interaction.user.displayAvatarURL({ dynamic: true, size: 1024 })
-      const avatarWEBP = await interaction.user.displayAvatarURL({ extension: "webp", size: 512 })
-      const avatarPNG = await interaction.user.displayAvatarURL({ extension: "png", size: 512 })
-      const avatarJPG = await interaction.user.displayAvatarURL({ extension: "jpg", size: 512 })
-      const avatarJPEG = await interaction.user.displayAvatarURL({ extension: "jpeg", size: 512 })
-      const dominantColor = await getColorFromURL(avatarPNG);
-      const avatarglobal = new Discord.EmbedBuilder()
-        .setAuthor({ name: interaction.user.username, iconURL: avatar })
-        .setDescription(`▸ 📷 Formatos\n> [WEBP](${avatarWEBP}) | [PNG](${avatarPNG}) | [JPG](${avatarJPG}) | [JPEG](${avatarJPEG}) \n\n▸ 🖌️ Color Dominante\n> HEX: ${rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2])}\n> RGB: ${dominantColor}\n\n▸ 📎 Fuentes\n>>> [Buscar en Google](https://lens.google.com/uploadbyurl?url=${avatar})\n[Buscar en Yandex](https://yandex.com/images/search?url=${avatar}&rpt=imageview)\n[Buscar en SauceNAO](https://saucenao.com/search.php?url=${avatar})`)
-        .setColor(dominantColor)
-        .setImage(avatar)
-      interaction.reply({ embeds: [avatarglobal] })
-    }
+    const avatar = await interaction.options.getUser('usuario').displayAvatarURL({ dynamic: true, size: 1024 }) // Avatar del usuario en cualquier formato dado por Discord
+
+    const avatarEmbed = new Discord.EmbedBuilder()
+      .setAuthor({ name: interaction.options.getUser('usuario').username, iconURL: avatar })
+      .setDescription(`▸ 📷 Formatos\n> [WEBP](${getAvatarWithExtension("webp")}) | [PNG](${getAvatarWithExtension("png")}) | [JPG](${getAvatarWithExtension("jpg")}) | [JPEG](${getAvatarWithExtension("jpeg")}) \n\n▸ 🖌️ Color Dominante\n> HEX: ${rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2])}\n> RGB: ${dominantColor}\n\n▸ 📎 Fuentes\n>>> [Buscar en Google](https://lens.google.com/uploadbyurl?url=${avatar})\n[Buscar en Yandex](https://yandex.com/images/search?url=${avatar}&rpt=imageview)\n[Buscar en SauceNAO](https://saucenao.com/search.php?url=${avatar})`)
+      .setColor(dominantColor)
+      .setImage(avatar)
+
+    interaction.reply({ embeds: [avatarEmbed] })
   }
 }
