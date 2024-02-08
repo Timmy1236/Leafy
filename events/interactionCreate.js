07c217f28a58c2bcd596b83d0b58a25f9d8c1e22
Timmy <timmy.1236@outlook.com>
2024-02-08 15:45:28 -0300

    Mejorado y fixeado un error fatal sobre el evento "interactionCreate"

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,13 +6,21 @@ module.exports = async (client, interaction) => {
   if (!slashCommand) return; // Si el comando no existe, lo ignoramos.
 
   try {
-    // Si el comando cuenta con permisos y el usuario no tiene los permisos necesarios, devolvemos un mensaje de error.
-    if (slashCommand.permissions && slashCommand.permissions.length > 0) {
-      if (!interaction.member.permissions.has(slashCommand.userPermissions)) return await interaction.reply({ content: `No tienes el permiso ${slashCommand.userPermissions} para ejecutar este comando.`, ephemeral: true });
+    // Juntamos todos los permisos necesarios en un string separado por comas.
+    let permisos = slashCommand.permisos.map(permiso => { return `\`${permiso}\``; }).join(", ");
+
+    if (slashCommand.data.default_member_permissions && slashCommand.data.default_member_permissions.length > 0) {
+      // Checkeamos si el usuario tiene los permisos necesarios para ejecutar el comando
+      if (!interaction.member.permissions.has(slashCommand.data.default_member_permissions)) return await interaction.reply({ content: `No tienes el permiso **${permisos}** para ejecutar este comando.`, ephemeral: true });
+      // Checkeamos si el bot también tiene los permisos necesarios para ejecutar el comando
+      if (!interaction.guild.members.me.permissions.has(slashCommand.data.default_member_permissions)) return await interaction.reply({ content: `No tengo el permiso **${permisos}** para ejecutar este comando.`, ephemeral: true });
     }
+
     // Si el usuario cumple con los permisos necesarios, ejecutamos el comando.
     await slashCommand.run(client, interaction);
+
   } catch (error) { // Si ocurre un error al ejecutar el comando, devolvemos un mensaje de error y lo registramos en la consola para el debug.
+
     await interaction.reply({ content: "Acaba de ocurrir un error al ejecutar el comando.", ephemeral: true });
     console.log("Acaba de ocurrir un error al intentar ejecutar un slash-command")
     console.error(error);
