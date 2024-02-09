d8b19f3aecc95a06e1da8bb106a75c2a97c0543f
Timmy <timmy.1236@outlook.com>
2024-02-09 19:26:55 -0300

    Nuevo emojis

--- src/emojis.js
+++ src/emojis.js
@@ -1,3 +1,7 @@
 module.exports = {
-  discord: "<:Leafy_Discord:1104269881412685894>",
+  discord: "<:Leafy_DiscordLogo:1146184569373073510>",
+  warning: "<:Leafy_Warning:1205636943484952607>",
+  error: "<:Leafy_Error:1205636971297505351>",
+  success: "<:Leafy_Success:1205636959293149245>",
 };
+// https://drive.google.com/uc?export=download&id=15C2VaqzpCFztpiquzDmSMov-O5CPFJUR
